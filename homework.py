2be36e3ae662a74596ddd4a2597525f88fd1c6be
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2022-09-13 17:56:23 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,23 +1,15 @@
 from dataclasses import dataclass
-from typing import Union, List
+from typing import Union, List, ClassVar
 
 
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float,
-                 ) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (f"Тип тренировки: {self.training_type};"
@@ -29,11 +21,12 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP: float = 0.65
-    M_IN_KM: float = 1000
+    LEN_STEP: ClassVar[float] = 0.65
+    M_IN_KM: ClassVar[int] = 1000
+    MIN_IN_H: ClassVar[int] = 60
 
     def __init__(self,
-                 action: float,
+                 action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
@@ -51,7 +44,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        return 0
+        raise NotImplementedError
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -64,25 +57,23 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_COFF_1 = 18
-    CALORIES_COFF_2 = 20
-    H_IN_MIN = 60
+    CALORIES_COFF_1: ClassVar[float] = 18
+    CALORIES_COFF_2: ClassVar[float] = 20
 
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_COFF_1 * self.get_mean_speed()
                 - self.CALORIES_COFF_2) * self.weight / self.M_IN_KM
-                * self.duration * self.H_IN_MIN)
+                * self.duration * self.MIN_IN_H)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    CALORIES_COFF_1 = 0.035
-    SQUARE_COFF = 2
-    CALORIES_COFF_2 = 0.029
-    H_IN_MIN = 60
+    CALORIES_COFF_1: ClassVar[float] = 0.035
+    SQUARE_COFF: ClassVar[float] = 2
+    CALORIES_COFF_2: ClassVar[float] = 0.029
 
     def __init__(self,
-                 action: float,
+                 action: int,
                  duration: float,
                  weight: float,
                  height: float,
@@ -93,17 +84,17 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_COFF_1 * self.weight + (self.get_mean_speed()
                 ** self.SQUARE_COFF // self.height) * self.CALORIES_COFF_2
-                * self.weight) * self.duration * self.H_IN_MIN)
+                * self.weight) * self.duration * self.MIN_IN_H)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP: float = 1.38
-    CALORIES_COFF_1 = 1.1
-    CALORIES_COFF_2 = 2
+    LEN_STEP: ClassVar[float] = 1.38
+    CALORIES_COFF_1: ClassVar[float] = 1.1
+    CALORIES_COFF_2: ClassVar[int] = 2
 
     def __init__(self,
-                 action: float,
+                 action: int,
                  duration: float,
                  weight: float,
                  length_pool: float,
@@ -126,12 +117,12 @@ def read_package(workout_type: str, data: List[Union[int, float]]) -> Union[
         Training, Union[Running, SportsWalking, Swimming]]:
     """Прочитать данные полученные от датчиков."""
 
-    DICT = {'SWM': Swimming,
-            'RUN': Running,
-            'WLK': SportsWalking,
-            }
+    TrainingMap = {'SWM': Swimming,
+                   'RUN': Running,
+                   'WLK': SportsWalking,
+                   }
 
-    return DICT[workout_type](*data)
+    return TrainingMap[workout_type](*data)
 
 
 def main(training: Training) -> None:
